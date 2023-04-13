f2313da15c17c33516bf1ade39bdd3d2522b6639
Valeriy Litvinov <valitvinoff@mail.ru>
2023-04-13 12:42:11 +0300

    subscription, onDestroy

--- src/app/components/todos/todos.component.ts
+++ src/app/components/todos/todos.component.ts
@@ -1,50 +1,63 @@
-import { Component, OnInit } from '@angular/core'
+import { Component, OnDestroy, OnInit } from '@angular/core'
 import { Todo, TodosService } from '../../services/todos.service'
 import { HttpErrorResponse } from '@angular/common/http'
+import { Subscription } from 'rxjs'
 
 @Component({
   selector: 'inst-todos',
   templateUrl: './todos.component.html',
   styleUrls: ['./todos.component.css'],
 })
-export class TodosComponent implements OnInit {
+export class TodosComponent implements OnInit, OnDestroy {
   todos: Todo[] = []
   error = ''
 
+  subscription: Subscription = new Subscription()
+
   constructor(private todosService: TodosService) {}
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe()
+  }
   ngOnInit(): void {
     this.getTodos()
   }
 
   getTodos() {
-    this.todosService.getTodos().subscribe({
-      next: res => {
-        this.todos = res
-      },
-      error: (err: HttpErrorResponse) => {
-        this.error = err.message
-      },
-    })
+    this.subscription.add(
+      this.todosService.getTodos().subscribe({
+        next: res => {
+          this.todos = res
+        },
+        error: (err: HttpErrorResponse) => {
+          this.error = err.message
+        },
+      })
+    )
   }
 
   createTodo() {
     const randomNumber = Math.floor(Math.random() * 100)
     const title = 'Angular' + randomNumber
-    this.todosService.createTodo(title).subscribe(res => {
-      const newTodo = res.data.item
-      this.todos.unshift(newTodo)
-    })
+    this.subscription.add(
+      this.todosService.createTodo(title).subscribe(res => {
+        const newTodo = res.data.item
+        this.todos.unshift(newTodo)
+      })
+    )
   }
 
   deleteTodo() {
     const todoId = '00f4d1c3-8875-4ed0-b7a1-5ff2eea4a186'
-    this.todosService.deleteTodo(todoId).subscribe({
-      next: () => {
-        this.todos = this.todos.filter(tl => tl.id !== todoId)
-      },
-      error: (err: HttpErrorResponse) => {
-        this.error = err.message
-      },
-    })
+    this.subscription.add(
+      this.todosService.deleteTodo(todoId).subscribe({
+        next: () => {
+          this.todos = this.todos.filter(tl => tl.id !== todoId)
+        },
+        error: (err: HttpErrorResponse) => {
+          this.error = err.message
+        },
+      })
+    )
   }
 }
