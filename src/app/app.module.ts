1404e0e441f650b7eb5561bfc22d9da7a199e45c
Valeriy Litvinov <valitvinoff@mail.ru>
2023-04-13 12:29:52 +0300

    init commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,16 @@ import { BrowserModule } from '@angular/platform-browser'
 
 import { AppComponent } from './app.component'
 import { FormsModule } from '@angular/forms'
-import { ParentComponent } from './parent/parent.component'
-import { ChildComponent } from './parent/child/child.component'
-import { NgSwitchComponent } from './ngswitch/ngswitch.component'
-import { NgClassComponent } from './ngclass/ngclass.component';
-import { NgforComponent } from './ngfor/ngfor.component'
+import { ParentComponent } from './components/parent/parent.component'
+import { ChildComponent } from './components/parent/child/child.component'
+import { NgSwitchComponent } from './components/ngswitch/ngswitch.component'
+import { NgClassComponent } from './components/ngclass/ngclass.component'
+import { NgforComponent } from './components/ngfor/ngfor.component'
+import { PipeComponent } from './components/pipe/pipe.component'
+import { ServiceComponent } from './components/service/service.component'
+import { Service2Component } from './components/service2/service2.component'
+import { TodosComponent } from './components/todos/todos.component'
+import { HttpClientModule } from '@angular/common/http'
 
 @NgModule({
   declarations: [
@@ -17,8 +22,12 @@ import { NgforComponent } from './ngfor/ngfor.component'
     NgSwitchComponent,
     NgClassComponent,
     NgforComponent,
+    PipeComponent,
+    ServiceComponent,
+    Service2Component,
+    TodosComponent,
   ],
-  imports: [BrowserModule, FormsModule],
+  imports: [BrowserModule, FormsModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
 })
---
+++ src/app/services/value.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core'
+import { BehaviorSubject } from 'rxjs'
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ValueService {
+  // value = 10
+  value$: BehaviorSubject<number> = new BehaviorSubject<number>(0)
+
+  add() {
+    this.value$.next(this.value$.getValue() + 1)
+    // this.value = this.value + 1
+  }
+
+  dec() {
+    this.value$.next(this.value$.getValue() - 1)
+    // this.value = this.value - 1
+  }
+}
